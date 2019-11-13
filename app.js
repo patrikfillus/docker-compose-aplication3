cc658a8ebdad0b23ee0449e1abf80dc8cd7b39f2
Patrik Fillus <patrik.fillus@live.com>
2019-11-13 00:13:38 -0300

    commit 3

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var http = require('http');
 var os = require('os');
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end(`Hostname: ${os.hostname()}\n
+    res.end(`
     {
         "nome": "Patrik",
         "cpf": "11916886908",
